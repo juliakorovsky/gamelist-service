ea0b1a7f2921ea702e0a19d6123900d5295a1cb7
Korovsky <sestra.che@gmail.com>
2018-08-12 21:33:46 +0300

    Added templates, added static/media dirs in  settings, added view for the front page

--- gamelist/views.py
+++ gamelist/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    template_name = 'start.html'
+    if request.user.is_authenticated():
+        template_name = 'loggedin.html'
+    return render(request, template_name)
--- my_service/settings.py
+++ my_service/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
