c3936cf4c9488fae4aa22d712d506a1c93b89519
Korovsky <sestra.che@gmail.com>
2018-08-16 01:28:06 +0300

    Changed relations between Game and Platform models

--- gamelist/admin.py
+++ gamelist/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from gamelist.models import Game, Platform
 
 # Register your models here.
+
+admin.site.register(Game)
+admin.site.register(Platform)
--- gamelist/models.py
+++ gamelist/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Platform(models.Model):
+    title = models.CharField(max_length=100, primary_key=True)
+    games = models.ManyToManyField('Game', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100, primary_key=True)
+    platforms = models.ManyToManyField('Platform', blank=False)
+
+    def __str__(self):
+        return self.title
