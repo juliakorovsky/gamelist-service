60a58c45da2447822a6e1b9e8b7bda9a576d119d
Korovsky <sestra.che@gmail.com>
2018-08-18 19:48:55 +0300

    Specified platform family for consoles

--- gamelist/models.py
+++ gamelist/models.py
@@ -3,8 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Platform(models.Model):
+    family_choices = (
+    ('NOT_SPECIFIED', 'Not specified'),
+    ('NIN', 'Nintendo'),
+    ('PS', 'Playstation'),
+    ('SEGA', 'Sega')
+    )
+
     title = models.CharField(max_length=100, primary_key=True)
     games = models.ManyToManyField('Game', blank=True)
+    platform_family = models.CharField(max_length=20, choices=family_choices, default='NOT_SPECIFIED')
 
     def __str__(self):
         return self.title
